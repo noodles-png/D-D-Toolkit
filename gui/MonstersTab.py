af5d397d21bd9c1fb3d96750b957a048724972d3
Patrick Carillo <p.carillo17@googlemail.com>
2026-05-05 08:43:58 +0200

    Initial commit

---
+++ gui/MonstersTab.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+from database.db_manager import DnDDatabase
+
+
+class MonstersTab:
+    def __init__(self, parent):
+        self.db = DnDDatabase()
+
+        self.left_frame = ctk.CTkFrame(parent)
+        self.left_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+        self.right_frame = ctk.CTkFrame(parent)
+        self.right_frame.pack(side="right", fill="both", expand=True, padx=10, pady=10)
+
+        self.editor_tabs = ctk.CTkTabview(self.right_frame)
+        self.editor_tabs.pack(fill="both", expand=True)
