dcd14488d8cc5e803e7e46ec1bc05275dac10add
Patrick Carillo <p.carillo17@googlemail.com>
2026-05-05 10:08:48 +0200

    Added feat: Scrollable frame

--- utils/helpers.py
+++ utils/helpers.py
@@ -31,10 +31,35 @@ def CollapsibleSection():
     """ Open/Closes Widget via Button CLick """
     pass
 
-
+# TODO Adjust code!!!
+# Code from https://github.com/TomSchimansky/CustomTkinter/blob/master/examples/scrollable_frame_example.py
 class Overview_Frame(ctk.CTkScrollableFrame):
-    def __init__(self, master, **kwargs):   # **kwargs for not yet defined var
+    def __init__(self, master, command=None, **kwargs):   # **kwargs for not yet defined var
         super().__init__(master, **kwargs)
+        self.grid_columnconfigure(0, weight=1)
 
+        self.command = command
+        self.radiobutton_variable = ctk.StringVar()
+        self.label_list = []
+        self.button_list = []
+
+    def add_item(self, item, image=None):
+        label = ctk.CTkLabel(self, text=item, image=image, compound="left", padx=5, anchor="w")
+        button = ctk.CTkButton(self, text="Command", width=100, height=24)
+        if self.command is not None:
+            button.configure(command=lambda: self.command(item))
+        label.grid(row=len(self.label_list), column=0, pady=(0, 10), sticky="w")
+        button.grid(row=len(self.button_list), column=1, pady=(0, 10), padx=5)
+        self.label_list.append(label)
+        self.button_list.append(button)
+
+    def remove_item(self, item):
+        for label, button in zip(self.label_list, self.button_list):
+            if item == label.cget("text"):
+                label.destroy()
+                button.destroy()
+                self.label_list.remove(label)
+                self.button_list.remove(button)
+                return
 
 
