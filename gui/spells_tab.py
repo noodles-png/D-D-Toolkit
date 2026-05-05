a2d482dfbcbd2abe48c63c5ba62df55eb57945b1
Patrick Carillo <p.carillo17@googlemail.com>
2026-05-05 23:23:43 +0200

    Added feat: spells tab prototype finished

--- gui/spells_tab.py
+++ gui/spells_tab.py
@@ -1,16 +1,59 @@
 import customtkinter as ctk
 from database.db_manager import DnDDatabase
-
+from utils.helpers import CollapsibleSection
 
 class SpellsTab:
     def __init__(self, parent):
         self.db = DnDDatabase()
 
-        self.left_frame = ctk.CTkFrame(parent)
+        self.left_frame = ctk.CTkScrollableFrame(parent)
         self.left_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
         self.right_frame = ctk.CTkFrame(parent)
         self.right_frame.pack(side="right", fill="both", expand=True, padx=10, pady=10)
 
-        self.editor_tabs = ctk.CTkTabview(self.right_frame)
-        self.editor_tabs.pack(fill="both", expand=True)
+        self.detail_box = ctk.CTkTextbox(self.right_frame, font=("Arial", 14))
+        self.detail_box.pack(fill="both", expand=True, padx=10, pady=10)
+
+        self.build_spells_list()
+
+    def build_spells_list(self):
+        title_names = ["Cantrips",
+                       "1st Level",
+                       "2nd Level",
+                       "3rd Level",
+                       "4th Level",
+                       "5th Level",
+                       "6th Level",
+                       "7th Level",
+                       "8th Level",
+                       "9th Level"
+                       ]
+        self.spell_sections = {}
+
+        for i, title in enumerate(title_names):
+            section = CollapsibleSection(self.left_frame, title)
+            self.spell_sections[i] = section
+
+        # TODO: Search bar above the spell sections
+        """
+        ctk.CTkLabel(self.left_frame, text="Spell Overview").grid(row=0, column=0, padx=10, pady=10)
+        ctk.CTkEntry(self.left_frame, text="Search Name").grid(row=1, column=0, padx=10, pady=10)
+        """
+
+        for level, section in self.spell_sections.items():
+            spells = self.db.get_spells_by_level(level)
+            for spell in spells:
+                btn = ctk.CTkButton(
+                    section.content,
+                    text=spell["spell_name"],
+                    command=lambda s=spell: self.show_spell_details(s)
+                )
+                btn.pack(fill="x", pady=1)
+
+    def show_spell_details(self, spell):
+        self.detail_box.configure(state="normal")
+        self.detail_box.delete("1.0", "end")
+        self.detail_box.insert("1.0",
+                               f"{spell['spell_name']}\nLevel {spell['spell_level']} {spell['spell_school']}\n\n{spell['description']}")
+        self.detail_box.configure(state="disabled")
 
--- utils/helpers.py
+++ utils/helpers.py
@@ -4,17 +4,20 @@ import customtkinter as ctk
 def get_asset(filename):
     return os.path.join(os.path.dirname(__file__), "..", "assets", filename)
 
+
 def get_modifier(value):
     """ Returns the calculated modifier based on the ability score """
     value = int(value)
     modifier = round((value - 10) // 2)
     return modifier
 
+
 def get_prof_bonus(level):
     """ Returns the proficiency bonus based on character level """
     level = int(level)
     prof_bonus = (level // 4) + 1
     return prof_bonus
+
 
 def get_spell_dc(prof_bonus, stat_modifier):
     """ Returns the spell difficulty check
@@ -27,39 +30,34 @@ def get_spell_dc(prof_bonus, stat_modifier):
     spell_dc = 8 + prof_bonus + stat_modifier
     return spell_dc
 
-def CollapsibleSection():
-    """ Open/Closes Widget via Button CLick """
-    pass
 
-# TODO Adjust code!!!
-# Code from https://github.com/TomSchimansky/CustomTkinter/blob/master/examples/scrollable_frame_example.py
-class Overview_Frame(ctk.CTkScrollableFrame):
-    def __init__(self, master, command=None, **kwargs):   # **kwargs for not yet defined var
-        super().__init__(master, **kwargs)
-        self.grid_columnconfigure(0, weight=1)
+class CollapsibleSection:
+    """ """
+    def __init__(self, parent, title):
+        self.title = title
+        self.is_open = False
 
-        self.command = command
-        self.radiobutton_variable = ctk.StringVar()
-        self.label_list = []
-        self.button_list = []
+        self.header_btn = ctk.CTkButton(parent, text=f"▶ {title}", command=self.toggle_button)
+        self.header_btn.pack(fill="x", pady=2)
 
-    def add_item(self, item, image=None):
-        label = ctk.CTkLabel(self, text=item, image=image, compound="left", padx=5, anchor="w")
-        button = ctk.CTkButton(self, text="Command", width=100, height=24)
-        if self.command is not None:
-            button.configure(command=lambda: self.command(item))
-        label.grid(row=len(self.label_list), column=0, pady=(0, 10), sticky="w")
-        button.grid(row=len(self.button_list), column=1, pady=(0, 10), padx=5)
-        self.label_list.append(label)
-        self.button_list.append(button)
+        self.content = ctk.CTkFrame(parent)
 
-    def remove_item(self, item):
-        for label, button in zip(self.label_list, self.button_list):
-            if item == label.cget("text"):
-                label.destroy()
-                button.destroy()
-                self.label_list.remove(label)
-                self.button_list.remove(button)
-                return
+    def toggle_button(self):
+        self.is_open = not self.is_open
+
+        if self.is_open:
+            self.content.pack()
+            self.header_btn.configure(text=f"▼ {self.title}")
+        else:
+            self.content.pack_forget()
+            self.header_btn.configure(text=f"▶ {self.title}")
+
+
+
+
+
+
+
+
 
 
